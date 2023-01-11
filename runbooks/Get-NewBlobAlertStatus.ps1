c5c9987c7bd3be518ad5c6a3bf17d1642f225c6f
Mike Dzikowski <micdz@microsoft.com>
2023-01-10 19:19:58 -0500

    update syntax

--- runbooks/Get-NewBlobAlertStatus.ps1
+++ runbooks/Get-NewBlobAlertStatus.ps1
@@ -14,20 +14,20 @@ catch
 }
 
 # Sleeping to ensure data is ingested into workspace
-while ($null -eq (($alert = Get-AzAlert | Where-Object {$_.Name -like "New Blob Uploaded to AVD*" -and $_.State -eq "New"} | Sort-Object -Property StartDateTime | Select-Object -Last 1 ))) {
+while ($null -eq (($alert = Get-AzAlert | Where-Object {$_.Name -like "New blob uploaded to container*" -and $_.State -eq "New"} | Sort-Object -Property StartDateTime | Select-Object -Last 1 ))) {
 Start-Sleep -Seconds 30
 }
 
 try
 {
     # Loop until the alert is closed
-    while ((($alert = Get-AzAlert | Where-Object {$_.Name -like "New Blob Uploaded to AVD*"} `
+    while ((($alert = Get-AzAlert | Where-Object {$_.Name -like "New blob uploaded to container*"} `
     | Sort-Object -Property StartDateTime | Select-Object -Last 1).State -eq "New") -and `
     ($null -eq ($comments = (Get-AzAlertObjectHistory -ResourceId $alert.id.split('/')[-1])[0] | Where-Object {$_.Comments -eq $null}))) {
         Start-Sleep -Seconds 5
     }
     Start-Sleep -Seconds 120
-    $alert = (Get-AzAlert | Where-Object {($_.Name -like "New Blob Uploaded to AVD*") -and ($_.State -eq "Closed")} `
+    $alert = (Get-AzAlert | Where-Object {($_.Name -like "New blob uploaded to container*") -and ($_.State -eq "Closed")} `
     | Sort-Object -Property StartDateTime | Select-Object -Last 1)
     $comments = (Get-AzAlertObjectHistory -ResourceId $alert.id.split('/')[-1]).Comments
     if($comments[0].contains("Approved")){
